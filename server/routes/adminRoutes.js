2f1ef6e390535dae38fb72b8fb59871aab836f4e
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2024-01-11 15:38:21 +0100

    10/01/24

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,5 +6,8 @@ router.post("/admin", adminControllers.createAdmin);
 
 router.post("/admin/login", adminControllers.loginAdmin);
 
+router.put('/admin/:id', adminControllers.updateAdmin)
+
+router.delete('/admin/:id', adminControllers.deleteAdmin)
 
 module.exports = router;
