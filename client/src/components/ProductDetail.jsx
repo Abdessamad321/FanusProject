fc4e2ec1a8b7c6fdfd01fef8e6081d5d8a3264df
oussamaom7 <oussama.maache@uit.ac.ma>
2024-02-17 20:23:15 +0100

    yui

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -6,6 +6,7 @@ import Ruban from '../assets/ruban.png';
 import R from '../assets/r.jpg';
 import CountdownTimer from './CountdownTimer ';
 import QuantitySelector from './QuantitySelector';
+import Info from './info';
 
 export default function ProductDetail() {
 
@@ -34,18 +35,20 @@ export default function ProductDetail() {
         </div>
         <div className="product flex mx-8">
             <div className="productimg w-1/2 ">
-                <img src={R} />
+                <img className=" h-3/5 w-full" src={R} alt="" />
             </div>
-            <div className="productinfo border-t-2 ">
-                <div className="quiqly flex ms-36">
-                    <h2 className="text-lg text-red-500">Quickly !! Buy your tickets quickly</h2>
+            <div className="productinfo border-t-2 ml-28 py-8">
+                <div className="quiqly flex ms-36 ">
+                    <h2 className="text-lg text-red-500 ml-[-4rem] mb-4">Quickly !! Buy your tickets quickly</h2>
                 </div>
-                <div className="chnoro">
+                <div className="chnoro mb-12">
                 <CountdownTimer eventDate={eventDate} />
                 </div>
-                <div className="infos"></div>
-                <div className="last flex">
-                    <div className="num">
+                <div className="infos mb-12">
+                    <Info />
+                </div>
+                <div className="last mb-6 flex space-x-4">
+                    <div className="num ">
                         <QuantitySelector />
                     </div>
                     <div className="addto">
---
+++ client/src/components/info.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import cal from "../assets/calendrier.png";
+import loc from "../assets/localisation.png";
+import tem from "../assets/temps.png";
+import liv from "../assets/livre.png";
+import can from "../assets/canape.png"
+
+export default function Info() {
+    const specificDate = new Date(2024, 2, 1);
+    const formattedDate = specificDate.toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleOptionSelect = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div className='info flex'>
+            <div className="q mr-4"> {/* Added mr-4 for right margin */}
+                <div className="cal flex mb-2">
+                    <img className='w-6 h4' src={cal}/>
+                    <p>Date & Time</p>
+                </div>
+                <div className="cal flex mb-2">
+                    <img className='w-6 h4' src={loc}/>
+                    <p>Localisation</p>
+                </div>
+                <div className="cal flex mb-2">
+                    <img className='w-6 h4' src={tem}/>
+                    <p>Duration</p>
+                </div>
+                <div className="cal flex mb-2">
+                    <img className='w-6 h4' src={liv}/>
+                    <p>Language</p>
+                </div>
+                <div className="cal flex mb-2">
+                    <img className='w-6 h4' src={can}/>
+                    <p className=''>Title Gorem Dolar Sit Amet</p>
+                </div>
+            </div>
+            <div className="a">
+                <p className="mb-2">{formattedDate}</p>
+                <p className='mb-2'>Room 4, Riad Leorm</p>
+                <p className='mb-2'>1hour 15minutes</p>
+                <p className='mb-2'>English</p>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    list="suggestions"
+                    onBlur={handleOptionSelect} 
+                    placeholder='Prices'
+                />
+                <datalist id="suggestions"> {/* Define datalist with id */}
+                    <option value="14$"></option> {/* Add suggestion options */}
+                    <option value="30$"></option>
+                    <option value="50$"></option>
+                </datalist>
+            </div>
+        </div>
+    )
+}
